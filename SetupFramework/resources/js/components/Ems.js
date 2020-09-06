19e32fcddb88490b4e394006cbb908fc084d0724
Emilio <A01351355@itesm.mx>
2020-09-06 00:52:25 -0500

    Agregar view Ems

---
+++ SetupFramework/resources/js/components/Ems.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Ems() {
+    return (
+        <div className="shadow p-3 mb-5 bg-white rounded" style={{marginTop: 40}}>
+            <h1 className="text-center">Emilio Aguilera Carlín</h1>
+            <h2 className="text-center">A01351355</h2>
+            <img src='storage/ems.jpg' alt="Foto de Emilio Aguilera" height="240px" width="240px" className="rounded mx-auto d-block" />
+            <hr/>
+            <h2>Experiencia en desarrollo web</h2>
+            <p>
+                Mi experiencia en desarrollo web consiste en dominio de lenguajes para front-end (HTML5, CSS, JS), y para back-end domino el lenguaje PHP. <br/>
+                Tengo experiencia on manejo de bases de datos en MySQL. <br/>
+                No tengo experiencia con ningun framework, pero he utilizado librerías de CSS como Bootstrap, Materialize y UIKit.
+            </p>
+        </div>
+    )
+}
+
+export default Ems
