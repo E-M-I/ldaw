b8ca40b83a5c58e8f3e4a7f2a627585e30eb174e
Ivan Diaz <A01700486@itesm.mx>
2020-09-08 18:10:03 -0500

    Cambios en la implementación del sistema

--- SetupFramework/resources/views/ivan.blade.php
+++ SetupFramework/resources/views/ivan.blade.php
@@ -8,6 +8,19 @@
 </head>
 <body>
     <div id="profile" class="container">
+        <div class="shadow p-3 mb-5 bg-white rounded" style="margin-top = 40">
+                <h1 class="text-center">Iván Alejandro Díaz Peralta</h1>
+                <h2 class="text-center">A01700486</h2>
+                <img src="<?= url('/img/ivan.jpg'); ?>" alt="Foto de Iván Díaz" height="240px" width="340px" class="rounded mx-auto d-block" />
+                <hr/>
+                <h2>Experiencia en desarrollo web</h2>
+                <p>
+                    Mi experiencia en desarrollo web consiste en el uso de HTML5, CSS, JS, y  PHP. <br/>
+                    MySQL es lo únco que tengo de experiencia en cuanto a bases de datos. <br/>
+                    No cuento con experiencia en el uso de frameworks, pero he utilizado librerías de CSS como Materialize.
+                </p>
+            </div>
+        </div>
     </div>
 </body>
 <script type="text/javascript" src="js/app.js"></script>
--- SetupFramework/routes/web.php
+++ SetupFramework/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/mau', function () {
     return view('mau');
 });
+
+Route::get('/ivan', function () {
+    return view('ivan');
+});
