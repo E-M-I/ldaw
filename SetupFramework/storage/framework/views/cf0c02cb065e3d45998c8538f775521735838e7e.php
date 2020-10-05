2dd4542df89ad917288b7e71b0b24672158f0ced
maualvm@gmail.com <maualvm@gmail.com>
2020-10-05 17:48:14 -0500

    Eliminar demo react

---
+++ SetupFramework/storage/framework/views/cf0c02cb065e3d45998c8538f775521735838e7e.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Mauricio Alvarez Milán</title>
+</head>
+<body>
+    <div id="profile" class="container">
+        <div class="shadow p-3 mb-5 bg-white rounded" style="margin-top = 40">
+                <h1 class="text-center">Mauricio Alvarez Milán</h1>
+                <h2 class="text-center">A01704948</h2>
+                <img src="<?= url('/img/mau.jpg'); ?>" alt="Foto de Mauricio Alvarez" height="240px" width="240px" class="rounded mx-auto d-block" />
+                <hr/>
+                <h2>Experiencia en desarrollo web</h2>
+                <p>
+                    Mi experiencia en desarrollo web consiste en dominio de lenguajes para front-end (HTML5, CSS, JS), y para back-end domino el lenguaje PHP. <br/>
+                    Tengo experiencia on manejo de bases de datos en MySQL. <br/>
+                    No tengo experiencia con ningun framework, pero he utilizado librerías de CSS como Bootstrap, Materialize y UIKit.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+<script type="text/javascript" src="js/app.js"></script>
+</html><?php /**PATH C:\Users\Mauricio\Documents\Tec\5to\Bloque2\LDAW\ldaw\SetupFramework\resources\views/mau.blade.php ENDPATH**/ ?>
--- demoReact/src/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function Footer() {
-    return(
-        <footer style={{backgroundColor: "lightGreen", fontSize: 14}}>Este es mi footer</footer>
-    )
-}
-export default Footer
