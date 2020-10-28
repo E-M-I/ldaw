45c5e3bf5d2b7e5e3016f9c2d7716cfa17b375b3
maualvm@gmail.com <maualvm@gmail.com>
2020-10-27 18:25:37 -0600

    Agregar modelos, controllers y rutas API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\OfertaController;
+use App\Http\Controllers\TituloController;
+use App\Http\Controllers\UsuarioTituloController;
+use App\Http\Controllers\UsuarioJuegoController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +23,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resources([
+    'ofertas' => OfertaController::class,
+    'titulos' => TituloController::class,
+    'intereses' => UsuarioTituloController::class,
+    'juegos' => UsuarioJuegoController::class,
+    'users' => UserController::class
+]);
+
+Route::get('coincidencias', 'App\Http\Controllers\UsuarioJuegoController@indexCoincidencias')->name('coincidencias');
+Route::get('ofertas/realizadas/{realizadas}', 'App\Http\Controllers\OfertaController@indexRealizadas')->name('realizadas');
+Route::get('ofertas/recibidas/{recibidas}', 'App\Http\Controllers\OfertaController@indexRecibidas')->name('recibidas');
