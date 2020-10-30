fc3816c31ef12d486eb912aa3aa6db1ef08ffc02
Ivan Diaz <A01700486@itesm.mx>
2020-10-30 01:57:10 -0600

    Se agregó el componente para registrar títulos

---
+++ frontend/src/componentes/RegistrarTitulo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class RegistrarTitulo extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Hola</p>
+            </div>
+        )
+    }
+
+}
+
+export default RegistrarTitulo;
