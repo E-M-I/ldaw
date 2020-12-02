939b0118a7a801a381d1b827277a48d8e7e4c89a
maualvm@gmail.com <maualvm@gmail.com>
2020-12-01 22:06:17 -0600

    Terminar frontend y terminar caso de uso

--- frontend/src/componentes/Juegos/RegistrarJuegoForm.js
+++ frontend/src/componentes/Juegos/RegistrarJuegoForm.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom';
 import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, FormControl, InputLabel, ListSubheader, MenuItem, Select, TextField } from '@material-ui/core'
+import Swal from 'sweetalert2';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,10 +18,13 @@ const useStyles = makeStyles((theme) => ({
 
 function RegistrarJuegoForm(props) {
     const classes = useStyles();
-    const [titulos, setTitulos] = useState();
-    const [consolas, setConsolas] = useState();
-    const [usuarios, setUsuarios] = useState();
+    const [titulos, setTitulos] = useState([]);
+    const [consolas, setConsolas] = useState([]);
+    const [usuarios, setUsuarios] = useState([]);
     const [nombre, setNombre] = useState();
+    const [tituloSelected, setTituloSelected] = useState('');
+    const [consolaSelected, setConsolaSelected] = useState('');
+    const [usuarioSelected, setUsuarioSelected] = useState('');
 
     useEffect (() => {
         //Obtener Titulos
@@ -29,7 +33,7 @@ function RegistrarJuegoForm(props) {
             setTitulos(res.data);
         })
         .catch((e) => {
-            console.log(e)
+            console.log(e);
         })
 
         //Obtener Consolas
@@ -38,7 +42,7 @@ function RegistrarJuegoForm(props) {
             setConsolas(res.data);
         })
         .catch((e) => {
-            console.log(e)
+            console.log(e);
         })
 
         //Obtener Usuarios
@@ -47,87 +51,102 @@ function RegistrarJuegoForm(props) {
             setUsuarios(res.data);
         })
         .catch((e) => {
-            console.log(e)
+            console.log(e);
         })
     }, []);
 
     const handleNombre = (event) => {
-        setNombre(event.target.value)
+        setNombre(event.target.value);
     }
 
     const handleTitulo = (event) => {
-        setTitulos(event.target.value)
+        let idTitulo = event.target.value;
+        setTituloSelected(event.target.value);
     }
 
     const handleConsola = (event) => {
-        setConsolas(event.target.value)
+        let idConsola = event.target.value;
+        setConsolaSelected(idConsola);
     }
 
     const handleUsuario = (event) => {
-        setUsuarios(event.target.value)
+        let idUsuario = event.target.value;
+        setUsuarioSelected(idUsuario);
     }
 
     const handleSubmit = (event) => {
-        const values = {
+        const juego = {
             nombre: nombre,
-            idTitulo: titulos.id,
-            idConsolas: consolas.id,
-            idUsuario: usuarios.id
+            idTitulo: tituloSelected,
+            idConsolas: consolaSelected,
+            idUsuario: usuarioSelected
         }
-        axios.post('http://localhost:8000/api/juegos', values, {headers: {"Accept": "application/json"}})
-            .then(res => {
-
+        axios.post('http://localhost:8000/api/juegos', juego)
+            .then(response => {
+                Swal.fire('¡Listo!', 'Tu juego ha sido registrado', 'success')
+                    .then(() => (
+                        window.location = "http://localhost:3000/register/game"
+                    ))
+            
             })
-            .catch(err => {
-                console.log(err)
-                props.history.push("/");
-
-            });
+            .catch(error => {
+                Swal.fire(
+                    '¡Error!',
+                    'Hubo un error al tratar de guardar tu juego. Por favor verifica que hayas llenado todos los campos.',
+                    'error'
+                    ).then(() => (
+                        window.location = "http://localhost:3000/register/game"
+                ));
+            })
     }
 
     return (
         <form className={classes.form}>
             <TextField id="nombre-juego" label="Nombre del juego" value={nombre} onChange={handleNombre}/><br/>
-            <FormControl className={classes.formControl}>
+            {/*
+                SELECT DE TÍTULOS
+            */}
+            <FormControl className={classes.formControl} >
                 <InputLabel htmlFor="grouped-select">Títulos</InputLabel>
-                <Select defaultValue="" id="grouped-select" onChange={titulos ?? handleTitulo}>
-                { //MAP TITULOS
-                    titulos ?
+                <Select defaultValue="" id="select-titulos" onChange={handleTitulo} name="select-titulos">
+                {
                     titulos.map((item) => (
-                            <MenuItem value={item.id}>{item.nombre}</MenuItem>
-                            )):
-                            <div/> 
-                        }
+                        <MenuItem value={item.id}>{item.nombre}</MenuItem>
+                    ))
+                }
                 </Select>
-            </FormControl><br/>
+            </FormControl>
+
+            {/*
+                SELECT DE CONSOLAS
+            */}
             <FormControl className={classes.formControl}>
                 <InputLabel htmlFor="grouped-select">Consolas</InputLabel>
-                <Select defaultValue="" id="grouped-select" onChange={consolas ?? handleConsola}>
-                { //MAP Consolas
-                    consolas ?
+                <Select defaultValue="" id="select-consolas" onChange={handleConsola} name="select-consolas">
+                {
                     consolas.map((item) => (
-                        <MenuItem value={item.id}>{item.nombre}</MenuItem>
-                    )):
-                    <div/>
-                                
+                        <MenuItem value={item.id} key={item.id}>{item.nombre}</MenuItem>
+                    ))
                 }
                 </Select>
-            </FormControl><br/>
+            </FormControl>
+
+            {/*
+                SELECT DE USUARIOS
+            */}
             <FormControl className={classes.formControl}>
                 <InputLabel htmlFor="grouped-select">Usuarios</InputLabel>
-                <Select defaultValue="" id="grouped-select" onChange={usuarios ?? handleUsuario}>
-                { //MAP TITULOS
-                    usuarios ?
+                <Select defaultValue="" id="select-usuarios" onChange={handleUsuario} name="select-usuarios">
+                {
                     usuarios.map((item) => (
-                        <MenuItem value={item.id}>{item.name}</MenuItem>
-                    )):
-                    <div/>         
+                        <MenuItem value={item.id} key={item.id}>{item.name}</MenuItem>
+                    ))
                 }
                 </Select>
-            </FormControl><br/>
-            <div style={{textAlign: 'center'}}>
+            </FormControl>
 
-            <Button variant="contained" color="primary" onClick={handleSubmit}>Registrar</Button>
+            <div style={{textAlign: 'center', marginTop: '15px'}}>
+                <Button variant="contained" color="primary" onClick={handleSubmit}>Registrar juego</Button>
             </div>
         </form>
     )
